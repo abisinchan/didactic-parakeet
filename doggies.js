cda860f3af92ab1b88b18007d434a627297be502
AE SChan <abigailsinchan@gmail.com>
2023-09-06 17:20:40 -0400

    went with static page, eradicated carousel

---
+++ doggies.js
@@ -0,0 +1,54 @@
+  // Initialize Swiper instances for each section
+  var swiper1 = new Swiper(".mySwiper.one", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination.one",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next.one",
+      prevEl: ".swiper-button-prev.one",
+    },
+  });
+
+  var swiper2 = new Swiper(".mySwiper.two", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination.two",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next.two",
+      prevEl: ".swiper-button-prev.two",
+    },
+  });
+
+  var swiper3 = new Swiper(".mySwiper.three", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination.three",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next.three",
+      prevEl: ".swiper-button-prev.three",
+    },
+  });
+
+
+
+
+
+
